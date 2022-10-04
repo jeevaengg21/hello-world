2863a674af5b82c878d3443cbc5fa5fc60e9555f
jeevanantham.a <jeevaengg21@gmail.com>
2022-10-04 08:42:33 +0530

    useState, useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+/**
+ * Objective of the component is to organize all the hello world app component
+ */
+import Welcome from "./Welcome"
+import Weather from "./Weather"
+import Population from "./Population"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <>
+            <Welcome></Welcome>
+            <Weather></Weather>
+            <Population></Population>
+        </>
+    )
 }
 
 export default App;
